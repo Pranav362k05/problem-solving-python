8a50ecfbe0187244e513110a3216bef8b4cc53c9
Pranav Krishna <pky20052023@gmail.com>
2026-07-31 10:59:07 +0530

    snippets to memorize

---
+++ Python Snippets to memorize/remember these.py
@@ -0,0 +1,42 @@
+# Python snippets worth memorizing
+
+# Frequency
+
+d = {}
+
+for x in arr:
+    d[x] = d.get(x,0)+1
+
+#Maximum
+
+ans = float("-inf")
+
+#Minimum
+
+ans = float("inf")
+
+#Sort
+
+arr.sort()
+
+#Reverse
+
+arr.reverse()
+
+#2D Array
+
+for row in arr:
+    for x in row:
+        ...
+
+#Sum
+
+sum(arr)
+
+#Maximum
+
+max(arr)
+
+#Minimum
+
+min(arr)
